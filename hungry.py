48e2b6dfbfa74f4de246cc1320f4b8d6651dc5f2
Nishchal <nishchal.bhardwaj@ontariotechu.net>
2019-11-09 13:39:42 -0500

    changes made

--- hungry.py
+++ hungry.py
@@ -1,1 +1,5 @@
 print("I am hungry")
+# git add adds the file to staging area
+# git commit commits the code to local version control database
+# git commit adds changes to local version control database
+# git push pushes the code to github
